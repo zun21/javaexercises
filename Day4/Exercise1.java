25603c9ae3eb31e7bb11917543396885ca237d24
Pan Zun <44962361+zun21@users.noreply.github.com>
2019-12-20 14:12:01 +0900

    1styearecec

---
+++ Day4/Exercise1.java
@@ -0,0 +1,8 @@
+public class Exercise1{
+    public static void main(String[] args){
+        double amountOfChicken = 5.5;
+        System.out.println(amountOfChicken);
+     double amountOfBacon = 0.1;
+     System.out.println(amountOfBacon);
+    }
+}
---
+++ Day4/Exercise2.java
@@ -0,0 +1,10 @@
+public class Exercise2{
+    public static void main(String[] args){
+        String firstName="Pan";
+        String lastName="Zun";
+        String myCountryName="Myanmar";
+        String myCityName="Mandalay";
+        System.out.println("My Name is "+firstName+" "+lastName+"."+"\nI am From "+ myCityName+ " in "+ myCountryName+".");
+    }
+}
+
